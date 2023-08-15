6ef2e05618fd7117dff494da6cf5809cd394ba74
VasysMS <vasiavasia10000@gmail.com>
2023-08-15 15:43:11 +0300

    1.13.14-BETA

--- src/main/java/vasys/worldban/WorldBan.java
+++ src/main/java/vasys/worldban/WorldBan.java
@@ -1,16 +1,13 @@
 package vasys.worldban;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerResourcePackStatusEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
@@ -33,7 +30,7 @@ public class WorldBan extends JavaPlugin implements Listener {
     }
 
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event){
+    public void onPlayerResourcePackStatusEvent(PlayerResourcePackStatusEvent event){
         Player player = event.getPlayer();
         String world = player.getWorld().getName();
         if (worlds.contains(world)) {
